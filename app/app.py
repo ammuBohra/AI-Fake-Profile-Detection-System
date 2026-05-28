94f92172fd234f5a2604626aecb4c45ac9974ce9
ammuBohra <gudiyabohra7@gmail.com>
2026-05-28 19:06:27 +0530

    Integrated Serialized ML Model with Flask Application

--- app/app.py
+++ app/app.py
@@ -1,21 +1,16 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 app = Flask(__name__)
 
 # Load dataset
 df = pd.read_csv("../dataset/fake_instagram.csv")
+
 print(df.columns)
 
-# Using only 3 columns
-X = df[["followers", "following", "posts"]]
-
-y = df["fake"]
-
-# Train model
-model = RandomForestClassifier()
-model.fit(X,y)
+# Load trained model
+model = pickle.load(open("../models/fake_profile_model.pkl", "rb"))
 
 @app.route("/")
 def home():
@@ -24,16 +19,37 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
 
+    profile_pic = int(request.form["profile_pic"])
+    nums_length_username = int(request.form["nums_length_username"])
+    fullname_words = int(request.form["fullname_words"])
+    nums_length_fullname = int(request.form["nums_length_fullname"])
+    name_equals_username = int(request.form["name_equals_username"])
+    description_length = int(request.form["description_length"])
+    external_URL = int(request.form["external_URL"])
+    private = int(request.form["private"])
+    posts = int(request.form["posts"])
     followers = int(request.form["followers"])
-
     following = int(request.form["following"])
 
-    posts = int(request.form["posts"])
 
-    input_data = [[followers, following, posts]]
+    input_data = [[
+        profile_pic,
+        nums_length_username,
+        fullname_words,
+        nums_length_fullname,
+        name_equals_username,
+        description_length,
+        external_URL,
+        private,
+        posts,
+        followers,
+        following
+    ]]
 
     prediction = model.predict(input_data)
+    
     probability = model.predict_proba(input_data)
+    
     confidence = round(max(probability[0]) * 100, 2)
 
     if prediction[0] == 1:
