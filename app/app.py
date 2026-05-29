d3c0347a1f55e27fa25cc7d79cfc3d14fe37208a
ammuBohra <gudiyabohra7@gmail.com>
2026-05-29 18:24:14 +0530

    Added AI dashboard and analytics interface

--- app/app.py
+++ app/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 import pandas as pd
 import pickle
 
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
 app = Flask(__name__)
 
 # Load dataset
@@ -9,12 +12,51 @@ df = pd.read_csv("../dataset/fake_instagram.csv")
 
 print(df.columns)
 
+# Features and target
+X = df.drop("fake", axis=1)
+
+y = df["fake"]
+
 # Load trained model
 model = pickle.load(open("../models/fake_profile_model.pkl", "rb"))
 
+# Split data for accuracy calculation
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Predict on test data
+y_pred = model.predict(X_test)
+
+# Accuracy 
+accuracy = round(accuracy_score(y_test, y_pred) * 100, 2)
+
+# Dataset statistics
+total_profiles = len(df)
+
+fake_profiles = len(df[df["fake"] == 1])
+
+real_profiles = len(df[df["fake"] == 0])
+
+fake_percentage = round((fake_profiles / total_profiles) *100, 2)
+
+real_percentage = round((real_profiles / total_profiles) *100, 2)
+
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        accuracy = accuracy,
+        total_profiles = total_profiles,
+        fake_percentage = fake_percentage,
+        real_percentage = real_percentage)
+
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -57,7 +99,14 @@ def predict():
     else: 
         result = f"Genuine Profile ({confidence}% Confidence)"
     
-    return render_template("index.html", prediction_text = result)
+    return render_template(
+        "index.html",
+        prediction_text = result,
+        accracy = accuracy,
+        total_profiles = total_profiles,
+        fake_percentage = fake_percentage,
+        real_percentage = real_percentage)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
